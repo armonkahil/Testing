798b6d1949c77d1a8dcc6e83edac8d65c1995d29
armonkahil <armonkahil@gmail.com>
2020-03-17 17:16:49 -0400

    added wallaby, jest, enzyme, eslint, prettier, babel, and react

---
+++ react/ch2/wallaby.js
@@ -0,0 +1,9 @@
+module.exports = ​ function​() {​ 
+  ​ return​ {
+    ​​ testFramework: ​ 'jest'​,
+     ​ ​ env: {​​ type: ​ 'node'​,
+      ​  },
+       ​ ​​tests: [​'tests/**/*.test.js'​],
+        ​​files: [​'**/*.js'​, ​'!node_modules/**/*'​, ​'!**/*.test.js'​, ​'!**/.*'​],
+         ​  };
+         ​  };
